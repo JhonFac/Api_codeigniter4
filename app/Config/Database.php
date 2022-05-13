3a52c8dad9e684c1619ea83b016e1efe78577b50
Jhon Bohorquez <jhon.bohorquez@ariadnacg.com>
2022-05-13 09:27:38 -0500

    upload database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'banco',
+        'database' => 'servicioa',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
